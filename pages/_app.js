41f05cc1adf4459b25f431028df6ffe84a5c5323
Sebastian <sep.gil@gmail.com>
2022-05-23 15:31:57 +0200

    Add basic storyblok setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css'
+import { storyblokInit, apiPlugin } from '@storyblok/react';
+
+storyblokInit({
+  accessToken: process.env.STORYBLOK_API_KEY,
+  use: [apiPlugin]
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import {
+  useStoryblokState,
+  getStoryblokApi,
+  StoryblokComponent,
+} from "@storyblok/react";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -66,4 +72,25 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  let slug = "home";
+
+  let sbParams = {
+    version: "draft", // or 'published'
+  };
+
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+
+ 
+
+  return {
+    props: {
+      story: data ? data.story : false,
+      key: data ? data.story.id : false,
+    },
+    revalidate: 3600,
+  };
 }
